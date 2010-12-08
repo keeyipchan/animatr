d46e2db7ce4404fbb3ba8c18c93731d21a6aef89
Jeremy Kahn <jeremyckahn@gmail.com>
2010-12-08 00:09:08 -0600

    Added draw list functionality.

--- public/animatr/resources/capi/capi.js
+++ public/animatr/resources/capi/capi.js
@@ -10,15 +10,23 @@ function capi(canvas, params){
 	
 	var version = '0.1';
 	
+	// Strip the 'px' from a style string and add it to the element directly
+	function setDimensionVal(dim){
+		this[dim] = this.style[dim].replace(/px/gi, '') || this.params[dim];
+	}
+	
 	return {
 		init: function(canvas, params){
 			var style, capi, p;
 			
 			capi = canvas.capi = this;
 			capi.params = this.params = params || {};
+			capi.el = canvas;
+			capi.ctx = canvas.getContext('2d');
 			
 			// Set some default values
 			capi.params.fRate = capi.params.fRate || 20;
+			capi._drawList = [];
 			
 			capi.state = this.state = {
 				fCount: 0
@@ -29,6 +37,11 @@ function capi(canvas, params){
 					canvas.style[style] = capi.params.styles[style];
 				}
 			}
+			
+			// The height and width of the canvas draw area do not sync
+			// up, so set those manually here
+			capi.params.styles.height && setDimensionVal.call(this.el, 'height');
+			capi.params.styles.width && setDimensionVal.call(this.el, 'width');
 			
 			return canvas;
 		},
@@ -51,6 +64,7 @@ function capi(canvas, params){
 			this.state.fCount++;
 			
 			this.updateHandle = setTimeout(function(){
+				self.ctx.clearRect(0, 0, self.el.width, self.el.height);
 				self._update();
 				self.update();
 			}, 1000 / this.params.fRate);
@@ -59,7 +73,19 @@ function capi(canvas, params){
 		},
 		
 		_update: function(){
+			var i;
 			
+			for (i = 0; i < this._drawList.length; i++){
+				this._drawList[i].call(this, this.ctx);
+			}
+		},
+		
+		pop: function(){
+			return this._drawList.pop();
+		},
+		
+		push: function(func){
+			return this._drawList.push(func);
 		}
 	}.init(canvas, params);
 }
